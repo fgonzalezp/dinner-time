be944fbbfc6b0b55be822cdb57bdf29b876952d5
Freddy Gonzalez <freddy.gonzalezp.17@gmail.com>
2025-03-10 10:04:20 +0100

    Add bootstrap template and implement query methods for filtering

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,26 @@
 class RecipesController < ApplicationController
 
   def index
-    @recipes = Recipe.includes(:ingredients).paginate(page: params[:page], per_page: 12)
+    @tags= tags || []
+    @recipes =  if params[:full_match] == "yes"
+      Recipe.includes(:ingredients).with_all_ingredients(@tags)
+    else
+      Recipe.includes(:ingredients).with_any_ingredient(@tags)
+    end
+
+    @records_size = @recipes.count
+    @recipes = @recipes.paginate(page: params[:page], per_page: 9)
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  private
+
+  def tags
+    return if params["tags"].blank?
+
+    JSON.parse(params["tags"].to_s).map { |k| k["value"] }
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,39 @@
 class Recipe < ApplicationRecord
   belongs_to :category
   has_many :ingredients
+
+  scope :with_all_ingredients, ->(ingredient_terms) {
+    return all if ingredient_terms.blank?
+
+    terms = ingredient_terms.is_a?(Array) ? ingredient_terms : ingredient_terms.split(/\s*,\s*/)
+    terms = terms.map(&:strip).reject(&:blank?)
+
+    return all if terms.empty?
+
+    query = self
+
+    terms.each do |term|
+      query = query.where(id: Ingredient.where("ingredients.name ILIKE ?", "%#{term}%").select(:recipe_id))
+    end
+
+    query
+  }
+
+  scope :with_any_ingredient, ->(ingredient_terms) {
+    return all if ingredient_terms.blank?
+
+    terms = ingredient_terms.is_a?(Array) ? ingredient_terms : ingredient_terms.split(/\s*,\s*/)
+    terms = terms.map(&:strip).reject(&:blank?)
+
+    return all if terms.empty?
+
+    joins(:ingredients).where(
+      terms.map { |term| "ingredients.name ILIKE ?" }.join(" OR "),
+      *terms.map { |term| "%#{term}%" }
+    ).distinct
+  }
+
+  def formatted_ingredients
+    ingredients.map(&:name).join(", ")
+  end
 end
